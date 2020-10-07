8159a97e9c7d56549294d071829668a4642f3e9b
Eyal Betzalel <eyalbatzul@gmail.com>
2020-10-07 20:30:53 +0300

    Update viz.py

--- src/viz.py
+++ src/viz.py
@@ -22,7 +22,7 @@ def main(args):
   import ipdb; ipdb.set_trace()  
   clusters = np.load(args.color_cluster_path)
   samples = np.load(args.load_path)    
-  samples = [np.reshape(np.rint(127.5 * (clusters[s] + 1.0)), [32, 32, 3]).astype(np.uint8) for s in np.split(samples)]
+  samples = [np.reshape(np.rint(127.5 * (clusters[s] + 1.0)), [32, 32, 3]).astype(np.uint8) for s in np.hsplit(samples,1)[0]]
   
   if not os.path.exists(args.save_path):
     os.makedirs(args.save_path)
